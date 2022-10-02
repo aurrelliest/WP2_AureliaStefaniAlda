e21cd22fb4d83ba6e71b3fc049f99b0ac89780e1
aurrelliest <aureliastefani180@gmail.com>
2022-10-02 20:44:07 +0700

    upload tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    51
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Aurelia</li>
+        <li>Nama Tengah : Stefani</li>
+        <li>Nama Belakang : Alda</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Pariit Madura Blok B-10</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Landau</li>
+    </ul>
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Nonton Drama</li>
+        <li>Tidur</li>
+    </ul>
+</section>
